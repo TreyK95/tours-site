3782bf7a48517fa7ee822b9fdbf16da4db04d352
Trey Kemp <kemptrey@outlook.com>
2021-06-24 12:20:37 -0600

    styling finished

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const container = document.querySelector(".container");
+
 document.querySelector(".open-navbar-icon").addEventListener("click", () => {
   container.classList.add("change");
 });
+
 document.querySelector(".close-navbar-icon").addEventListener("click", () => {
   container.classList.remove("change");
 });
